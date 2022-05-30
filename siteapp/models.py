b832b8278df49dde844e9999468d6f1f29c5c17e
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-05-30 17:06:55 +0900

    model 수정 및 연혁 페이지 다듬기

--- siteapp/models.py
+++ siteapp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Introduction_history(models.Model):
-    date = models.DateField(null=False)
-    content = models.TextField(null=True)
+    date = models.CharField(max_length=20, null=False)
+    content = models.TextField(null=False)
