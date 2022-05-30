83a2a9379f8c1d3d54488e8386c977357aa5a671
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-05-30 16:39:48 +0900

    bootstrap4 script 설정, header navbar 추가, 연혁 이동 페이지 추가

--- siteapp/models.py
+++ siteapp/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Introduction_guide(models.Model):
-    title = models.CharField(max_length=200, null=False)
-    image = models.ImageField(upload_to='introduction/', null=False)
-
 class Introduction_history(models.Model):
     date = models.DateField(null=False)
     content = models.TextField(null=True)
